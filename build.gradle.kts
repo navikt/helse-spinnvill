e339b1bd1f3e239b5413875ac3d579dfc5b593a7
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2023-10-30 13:22:41 +0100

    🚀 Deployer Spekulant til dev
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,4 +25,19 @@ tasks {
     test {
         useJUnitPlatform()
     }
+    withType<Jar> {
+        archiveBaseName.set("app")
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.AppKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists()) it.copyTo(file)
+            }
+        }
+    }
 }
