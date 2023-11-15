d75c937b305e1f8e4069a8e6a30118d3a1446a25
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2023-11-15 14:19:09 +0100

    🔊 Logg påbegynt behandlig av utkast_til_vedtak
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- src/main/kotlin/no/nav/helse/mediator/Mediator.kt
+++ src/main/kotlin/no/nav/helse/mediator/Mediator.kt
@@ -1,6 +1,7 @@
 package no.nav.helse.mediator
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import net.logstash.logback.argument.StructuredArguments.kv
 import no.nav.helse.db.Dao
 import no.nav.helse.kafka.MessageHandler
 import no.nav.helse.kafka.UtkastTilVedtakMessage
@@ -8,11 +9,22 @@ import no.nav.helse.modell.Sykefraværstilfelle
 import no.nav.helse.modell.avviksvurdering.Avviksvurdering
 import no.nav.helse.modell.avviksvurdering.Beregningsgrunnlag
 import no.nav.helse.rapids_rivers.RapidsConnection
+import org.slf4j.LoggerFactory
 import java.time.LocalDate
 
 class Mediator(rapidsConnection: RapidsConnection, private val dao: Dao): MessageHandler {
+    private companion object {
+        private val logg = LoggerFactory.getLogger(this::class.java)
+        private val sikkerlogg = LoggerFactory.getLogger("tjenestekall")
+    }
 
     override fun håndter(utkastTilVedtakMessage: UtkastTilVedtakMessage) {
+        logg.info("Behandler utkast_til_vedtak for {}", kv("vedtaksperiodeId", utkastTilVedtakMessage.vedtaksperiodeId))
+        sikkerlogg.info(
+            "Behandler utkast_til_vedtak for {}, {}",
+            kv("fødselsnummer", utkastTilVedtakMessage.fødselsnummer),
+            kv("vedtaksperiodeId", utkastTilVedtakMessage.vedtaksperiodeId)
+        )
         val beregningsgrunnlag = Beregningsgrunnlag(utkastTilVedtakMessage.beregningsgrunnlag)
         val sykefraværstilfelle = sykefraværstilfelle(utkastTilVedtakMessage.fødselsnummer, utkastTilVedtakMessage.skjæringstidspunkt, beregningsgrunnlag)
         sykefraværstilfelle.nyttUtkastTilVedtak(beregningsgrunnlag)
