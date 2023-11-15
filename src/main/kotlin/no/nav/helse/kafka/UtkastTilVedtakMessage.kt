4ea55f7c7490eb2a75c9f96dc0be3ba04b5bb23a
Øydis Kind Refsum <oydis.kind.refsum@nav.no>
2023-11-15 08:58:00 +0100

    ♻️ UtkastTilVedtakMessage kjenner ikke til modellobjekter
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- src/main/kotlin/no/nav/helse/kafka/UtkastTilVedtakMessage.kt
+++ src/main/kotlin/no/nav/helse/kafka/UtkastTilVedtakMessage.kt
@@ -1,17 +1,14 @@
 package no.nav.helse.kafka
 
-import no.nav.helse.modell.avviksvurdering.Beregningsgrunnlag
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.asLocalDate
 import java.time.LocalDate
 
 class UtkastTilVedtakMessage(packet: JsonMessage) {
 
-    val skjæringstidspunkt: LocalDate = packet["skjæringstidspunkt"].asLocalDate()
+    val skjæringstidspunkt: LocalDate = packet["Godkjenning.skjæringstidspunkt"].asLocalDate()
     val fødselsnummer: String = packet["fødselsnummer"].asText()
-    val beregningsgrunnlag = packet["omregnedeÅrsinntekter"].map {
+    val beregningsgrunnlag = packet["Godkjenning.omregnedeÅrsinntekter"].associate {
         it["organisasjonsnummer"].asText() to it["beløp"].asDouble()
-    }.let {
-        Beregningsgrunnlag(it.toMap())
     }
 }
