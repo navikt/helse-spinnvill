1d82241896b9e5aeb7c7746f627196fc8733e5ee
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-11-23 08:47:05 +0100

    🎨 Strukturer Avviksvurdering-daoen
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- spinnvill-db/src/main/kotlin/no/nav/helse/db/Avviksvurdering.kt
+++ spinnvill-db/src/main/kotlin/no/nav/helse/db/Avviksvurdering.kt
@@ -24,51 +24,58 @@ internal class Avviksvurdering {
         class EnAvviksvurdering(id: EntityID<UUID>) : UUIDEntity(id) {
             companion object : UUIDEntityClass<EnAvviksvurdering>(Avviksvurderinger)
 
+            val beregningsgrunnlag by EttBeregningsgrunnlag optionalReferrersOn Beregningsgrunnlag.avviksvurdering
+            val sammenligningsgrunnlag by EttSammenligningsgrunnlag referrersOn Sammenligningsgrunnlag.avviksvurdering
+
             var fødselsnummer by Avviksvurderinger.fødselsnummer
             var skjæringstidspunkt by Avviksvurderinger.skjæringstidspunkt
             var opprettet by Avviksvurderinger.opprettet
-            val beregningsgrunnlag by EttBeregningsgrunnlag optionalReferrersOn Beregningsgrunnlag.avviksvurdering
-            val sammenligningsgrunnlag by EttSammenligningsgrunnlag referrersOn Sammenligningsgrunnlag.avviksvurdering
         }
 
         private object Beregningsgrunnlag : UUIDTable(name = "beregningsgrunnlag") {
+            val avviksvurdering = optReference("avviksvurdering_ref", Avviksvurderinger)
+
             val organisasjonsnummer: Column<String> = varchar("organisasjonsnummer", 9)
             val inntekt: Column<Double> = double("inntekt")
-            val avviksvurdering = optReference("avviksvurdering_ref", Avviksvurderinger)
         }
 
         class EttBeregningsgrunnlag(id: EntityID<UUID>) : UUIDEntity(id) {
             companion object : UUIDEntityClass<EttBeregningsgrunnlag>(Beregningsgrunnlag)
 
+            var avviksvurdering by EnAvviksvurdering optionalReferencedOn Beregningsgrunnlag.avviksvurdering
+
             var organisasjonsnummer by Beregningsgrunnlag.organisasjonsnummer
             var inntekt by Beregningsgrunnlag.inntekt
-            var avviksvurdering by EnAvviksvurdering optionalReferencedOn Beregningsgrunnlag.avviksvurdering
         }
 
         private object Sammenligningsgrunnlag : UUIDTable(name = "sammenligningsgrunnlag") {
-            val organisasjonsnummer: Column<String> = varchar("organisasjonsnummer", 9)
             val avviksvurdering = reference("avviksvurdering_ref", Avviksvurderinger)
+
+            val organisasjonsnummer: Column<String> = varchar("organisasjonsnummer", 9)
         }
 
         class EttSammenligningsgrunnlag(id: EntityID<UUID>) : UUIDEntity(id) {
             companion object : UUIDEntityClass<EttSammenligningsgrunnlag>(Sammenligningsgrunnlag)
 
-            var organisasjonsnummer by Sammenligningsgrunnlag.organisasjonsnummer
             val inntekter by EnMånedsinntekt referrersOn Månedsinntekter.sammenligningsgrunnlag
             var avviksvurdering by EnAvviksvurdering referencedOn Sammenligningsgrunnlag.avviksvurdering
+
+            var organisasjonsnummer by Sammenligningsgrunnlag.organisasjonsnummer
         }
 
         private object Månedsinntekter : UUIDTable(name = "manedsinntekt") {
+            val sammenligningsgrunnlag = reference("sammenligningsgrunnlag_ref", Sammenligningsgrunnlag)
+
             val inntekt: Column<Double> = double("inntekt")
             val år: Column<Int> = integer("år")
             val måned: Column<Int> = integer("måned")
-            val sammenligningsgrunnlag = reference("sammenligningsgrunnlag_ref", Sammenligningsgrunnlag)
         }
 
         class EnMånedsinntekt(id: EntityID<UUID>) : UUIDEntity(id) {
             companion object : UUIDEntityClass<EnMånedsinntekt>(Månedsinntekter)
 
             var sammenligningsgrunnlag by EttSammenligningsgrunnlag referencedOn Månedsinntekter.sammenligningsgrunnlag
+
             var inntekt by Månedsinntekter.inntekt
             var år by Månedsinntekter.år
             var måned by Månedsinntekter.måned
