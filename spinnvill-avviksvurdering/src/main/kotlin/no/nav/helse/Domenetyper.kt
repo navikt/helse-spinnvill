1fe3e97b33f98981ecb1c5520607538213d6a062
Martin Solheim <martin.solheim@me.com>
2023-11-24 11:20:11 +0100

    🏷️ Legger til domenetyper

---
+++ spinnvill-avviksvurdering/src/main/kotlin/no/nav/helse/Domenetyper.kt
@@ -0,0 +1,13 @@
+package no.nav.helse
+
+@JvmInline
+value class Fødselsnummer(val value: String)
+
+@JvmInline
+value class Organisasjonsnummer(val value: String)
+
+@JvmInline
+value class InntektPerMåned(val value: Double)
+
+@JvmInline
+value class OmregnetÅrsinntekt(val value: Double)
