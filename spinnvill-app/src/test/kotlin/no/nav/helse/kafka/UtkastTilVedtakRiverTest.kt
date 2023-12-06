251794ba917a9f0ec772d7b7dbc838fe18867b83
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-12-06 10:14:15 +0100

    🐛 Ikke les inn egen republisering av godkjenningsbehovet
    Co-authored-by: Martin Solheim <martin.solheim@me.com>

--- spinnvill-app/src/test/kotlin/no/nav/helse/kafka/UtkastTilVedtakRiverTest.kt
+++ spinnvill-app/src/test/kotlin/no/nav/helse/kafka/UtkastTilVedtakRiverTest.kt
@@ -45,6 +45,12 @@ class UtkastTilVedtakRiverTest {
         assertEquals(0, messageHandler.messages.size)
     }
 
+    @Test
+    fun `leser ikke inn godkjenningsbehov markert med behandlingStartet`() {
+        testRapid.sendTestMessage(utkastTilVedtakJsonMedBehandlingStartet(AKTØRID, FØDSELSNUMMER, ORGANISASJONSNUMMER, skjæringstidspunkt))
+        assertEquals(0, messageHandler.messages.size)
+    }
+
     private fun utkastTilVedtakJson(
         aktørId: String,
         fødselsnummer: String,
@@ -121,6 +127,60 @@ class UtkastTilVedtakRiverTest {
               "vedtaksperiodeId": "d6a1575f-a241-4338-baea-26df557f7506",
               "tilstand": "AVVENTER_GODKJENNING",
               "utbetalingId": "db8ff403-6ea3-4ad2-bfdf-d876e28c5839",
+              "Godkjenning": {
+                "periodeFom": "$skjæringstidspunkt",
+                "periodeTom": "${skjæringstidspunkt.plusDays(30)}",
+                "skjæringstidspunkt": "$skjæringstidspunkt",
+                "vilkårsgrunnlagId": "87b9339d-a67d-49b0-af36-c93d6f9249ae",
+                "periodetype": "FØRSTEGANGSBEHANDLING",
+                "førstegangsbehandling": true,
+                "utbetalingtype": "UTBETALING",
+                "inntektskilde": "EN_ARBEIDSGIVER",
+                "orgnummereMedRelevanteArbeidsforhold": [],
+                "tags": [
+                  "EN_ARBEIDSGIVER",
+                  "ARBEIDSGIVERUTBETALING"
+                ],
+                "kanAvvises": true,
+                "omregnedeÅrsinntekter": [
+                  {
+                    "organisasjonsnummer": "$organisasjonsnummer",
+                    "beløp": 500000.0
+                  },
+                  {
+                    "organisasjonsnummer": "000000000",
+                    "beløp": 200000.20
+                  }
+                ]
+              },
+              "@id": "ba376523-62b1-49d7-8647-f902c739b634",
+              "@opprettet": "2018-01-01T00:00:00.000"
+            }
+        """.trimIndent()
+        return json
+    }
+    private fun utkastTilVedtakJsonMedBehandlingStartet(
+        aktørId: String,
+        fødselsnummer: String,
+        organisasjonsnummer: String,
+        skjæringstidspunkt: LocalDate
+    ): String {
+        @Language("JSON")
+        val json = """
+            {
+              "@event_name": "behov",
+              "@behovId": "c64a73be-7337-4f25-8923-94f355c23d76",
+              "@behov": [
+                "Godkjenning"
+              ],
+              "meldingsreferanseId": "b63537e5-ffd9-4e9b-930c-45b0ab602d66",
+              "aktørId": "$aktørId",
+              "fødselsnummer": "$fødselsnummer",
+              "organisasjonsnummer": "$organisasjonsnummer",
+              "vedtaksperiodeId": "d6a1575f-a241-4338-baea-26df557f7506",
+              "tilstand": "AVVENTER_GODKJENNING",
+              "utbetalingId": "db8ff403-6ea3-4ad2-bfdf-d876e28c5839",
+              "behandlingStartet": true,
               "Godkjenning": {
                 "periodeFom": "$skjæringstidspunkt",
                 "periodeTom": "${skjæringstidspunkt.plusDays(30)}",
