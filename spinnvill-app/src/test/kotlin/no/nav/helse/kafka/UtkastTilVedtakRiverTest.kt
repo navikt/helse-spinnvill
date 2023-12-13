e1b0034e64c38bf423a19ed36e202e288948f1cb
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2023-12-13 20:01:35 +0100

    ♻️ deduplisere json i test

--- spinnvill-app/src/test/kotlin/no/nav/helse/kafka/UtkastTilVedtakRiverTest.kt
+++ spinnvill-app/src/test/kotlin/no/nav/helse/kafka/UtkastTilVedtakRiverTest.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.kafka
 
+import com.fasterxml.jackson.databind.node.ObjectNode
 import no.nav.helse.helpers.januar
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.intellij.lang.annotations.Language
@@ -105,112 +106,32 @@ class UtkastTilVedtakRiverTest {
         return json
     }
 
+    private fun utkastTilVedtakJsonNode(
+        aktørId: String, fødselsnummer: String, organisasjonsnummer: String, skjæringstidspunkt: LocalDate
+    ) = utkastTilVedtakJson(aktørId, fødselsnummer, organisasjonsnummer, skjæringstidspunkt)
+        .let(objectMapper::readTree) as ObjectNode
+
     private fun utkastTilVedtakJsonMedLøsning(
         aktørId: String,
         fødselsnummer: String,
         organisasjonsnummer: String,
         skjæringstidspunkt: LocalDate
-    ): String {
-        @Language("JSON")
-        val json = """
-            {
-              "@event_name": "behov",
-              "@behovId": "c64a73be-7337-4f25-8923-94f355c23d76",
-              "@behov": [
-                "Godkjenning"
-              ],
-              "@løsning": {},
-              "meldingsreferanseId": "b63537e5-ffd9-4e9b-930c-45b0ab602d66",
-              "aktørId": "$aktørId",
-              "fødselsnummer": "$fødselsnummer",
-              "organisasjonsnummer": "$organisasjonsnummer",
-              "vedtaksperiodeId": "d6a1575f-a241-4338-baea-26df557f7506",
-              "tilstand": "AVVENTER_GODKJENNING",
-              "utbetalingId": "db8ff403-6ea3-4ad2-bfdf-d876e28c5839",
-              "Godkjenning": {
-                "periodeFom": "$skjæringstidspunkt",
-                "periodeTom": "${skjæringstidspunkt.plusDays(30)}",
-                "skjæringstidspunkt": "$skjæringstidspunkt",
-                "vilkårsgrunnlagId": "87b9339d-a67d-49b0-af36-c93d6f9249ae",
-                "periodetype": "FØRSTEGANGSBEHANDLING",
-                "førstegangsbehandling": true,
-                "utbetalingtype": "UTBETALING",
-                "inntektskilde": "EN_ARBEIDSGIVER",
-                "orgnummereMedRelevanteArbeidsforhold": [],
-                "tags": [
-                  "EN_ARBEIDSGIVER",
-                  "ARBEIDSGIVERUTBETALING"
-                ],
-                "kanAvvises": true,
-                "omregnedeÅrsinntekter": [
-                  {
-                    "organisasjonsnummer": "$organisasjonsnummer",
-                    "beløp": 500000.0
-                  },
-                  {
-                    "organisasjonsnummer": "000000000",
-                    "beløp": 200000.20
-                  }
-                ]
-              },
-              "@id": "ba376523-62b1-49d7-8647-f902c739b634",
-              "@opprettet": "2018-01-01T00:00:00.000"
-            }
-        """.trimIndent()
-        return json
-    }
+    ) = utkastTilVedtakJsonNode(aktørId, fødselsnummer, organisasjonsnummer, skjæringstidspunkt)
+        .med("@løsning" to "{}")
+        .let(objectMapper::writeValueAsString)
+
     private fun utkastTilVedtakJsonMedBehandlingStartet(
         aktørId: String,
         fødselsnummer: String,
         organisasjonsnummer: String,
         skjæringstidspunkt: LocalDate
-    ): String {
-        @Language("JSON")
-        val json = """
-            {
-              "@event_name": "behov",
-              "@behovId": "c64a73be-7337-4f25-8923-94f355c23d76",
-              "@behov": [
-                "Godkjenning"
-              ],
-              "meldingsreferanseId": "b63537e5-ffd9-4e9b-930c-45b0ab602d66",
-              "aktørId": "$aktørId",
-              "fødselsnummer": "$fødselsnummer",
-              "organisasjonsnummer": "$organisasjonsnummer",
-              "vedtaksperiodeId": "d6a1575f-a241-4338-baea-26df557f7506",
-              "tilstand": "AVVENTER_GODKJENNING",
-              "utbetalingId": "db8ff403-6ea3-4ad2-bfdf-d876e28c5839",
-              "behandlingStartet": true,
-              "Godkjenning": {
-                "periodeFom": "$skjæringstidspunkt",
-                "periodeTom": "${skjæringstidspunkt.plusDays(30)}",
-                "skjæringstidspunkt": "$skjæringstidspunkt",
-                "vilkårsgrunnlagId": "87b9339d-a67d-49b0-af36-c93d6f9249ae",
-                "periodetype": "FØRSTEGANGSBEHANDLING",
-                "førstegangsbehandling": true,
-                "utbetalingtype": "UTBETALING",
-                "inntektskilde": "EN_ARBEIDSGIVER",
-                "orgnummereMedRelevanteArbeidsforhold": [],
-                "tags": [
-                  "EN_ARBEIDSGIVER",
-                  "ARBEIDSGIVERUTBETALING"
-                ],
-                "kanAvvises": true,
-                "omregnedeÅrsinntekter": [
-                  {
-                    "organisasjonsnummer": "$organisasjonsnummer",
-                    "beløp": 500000.0
-                  },
-                  {
-                    "organisasjonsnummer": "000000000",
-                    "beløp": 200000.20
-                  }
-                ]
-              },
-              "@id": "ba376523-62b1-49d7-8647-f902c739b634",
-              "@opprettet": "2018-01-01T00:00:00.000"
-            }
-        """.trimIndent()
-        return json
-    }
+    ) = utkastTilVedtakJsonNode(aktørId, fødselsnummer, organisasjonsnummer, skjæringstidspunkt)
+        .med("behandlingStartet" to "true")
+        .let(objectMapper::writeValueAsString)
+
+}
+
+private fun ObjectNode.med(vararg felter: Pair<String, Any>): ObjectNode {
+    felter.forEach { (key, value) -> replace(key, objectMapper.valueToTree(value)) }
+    return this
 }
