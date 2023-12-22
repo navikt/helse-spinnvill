2722dc09f287e384a4cd91de846d4dadb7b99491
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-12-22 12:36:28 +0100

    ♻️ BehovProducer er en BehovObserver

--- spinnvill-app/src/main/kotlin/no/nav/helse/mediator/producer/BehovProducer.kt
+++ spinnvill-app/src/main/kotlin/no/nav/helse/mediator/producer/BehovProducer.kt
@@ -2,10 +2,11 @@ package no.nav.helse.mediator.producer
 
 import com.fasterxml.jackson.databind.JsonNode
 import no.nav.helse.avviksvurdering.BehovForSammenligningsgrunnlag
+import no.nav.helse.avviksvurdering.BehovObserver
 
 internal class BehovProducer(
     private val utkastTilVedtakJson: JsonNode,
-): Producer {
+): Producer, BehovObserver {
     private val behovskø = mutableMapOf<String, Map<String, Any>>()
 
     override fun ferdigstill(): List<Message> {
@@ -21,8 +22,8 @@ internal class BehovProducer(
         behovskø.clear()
         return listOf(compositeBehov)
     }
-    internal fun sammenligningsgrunnlag(behovForSammenligningsgrunnlag: BehovForSammenligningsgrunnlag) {
-        behovskø["InntekterForSammenligningsgrunnlag"] = behovForSammenligningsgrunnlag.toMap()
+    override fun sammenligningsgrunnlag(behov: BehovForSammenligningsgrunnlag) {
+        behovskø["InntekterForSammenligningsgrunnlag"] = behov.toMap()
     }
 
     private fun BehovForSammenligningsgrunnlag.toMap(): Map<String, Any> {
