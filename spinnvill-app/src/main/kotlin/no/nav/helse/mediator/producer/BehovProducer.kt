ee58f68950155e72c2ca5fc35dfbd6f6093e0bd4
Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>
2023-12-22 12:37:09 +0100

    🎨 Litta newline kan vi koste på oss nå i jula

--- spinnvill-app/src/main/kotlin/no/nav/helse/mediator/producer/BehovProducer.kt
+++ spinnvill-app/src/main/kotlin/no/nav/helse/mediator/producer/BehovProducer.kt
@@ -22,6 +22,7 @@ internal class BehovProducer(
         behovskø.clear()
         return listOf(compositeBehov)
     }
+
     override fun sammenligningsgrunnlag(behov: BehovForSammenligningsgrunnlag) {
         behovskø["InntekterForSammenligningsgrunnlag"] = behov.toMap()
     }
