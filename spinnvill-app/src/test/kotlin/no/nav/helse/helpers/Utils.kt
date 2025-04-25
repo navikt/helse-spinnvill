c07fe5d0ed15f314f40de5b7be6c0f5f70629057
Christian Skovborg Gule <christianskovborg@gmail.com>
2025-04-25 14:01:36 +0200

    🔥 Fjern ubrukt metode

--- spinnvill-app/src/test/kotlin/no/nav/helse/helpers/Utils.kt
+++ spinnvill-app/src/test/kotlin/no/nav/helse/helpers/Utils.kt
@@ -1,6 +1,5 @@
 package no.nav.helse.helpers
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -42,8 +41,6 @@ internal fun sammenligningsgrunnlag(
         }
     )
 }
-
-internal fun Map<String, Any>.toJson() = objectMapper.convertValue(this, JsonNode::class.java)
 
 internal val objectMapper = jacksonObjectMapper()
     .registerModule(JavaTimeModule())
